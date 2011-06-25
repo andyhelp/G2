7319f06def9c65bed084ebf338c48d295cf543b4
Andrzej Bieniek <andyhelp@gmail.com>
2011-06-25 23:22:05 +0100

    Add init_db.py script. Instead using admin interface, modify the content and run it to set up database content.

---
+++ init_db.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file has been automatically generated, changes may be lost if you
+# go and generate it again. It was generated with the following command:
+# manage.py dumpscript playlist
+
+import datetime
+from decimal import Decimal
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+import os
+
+def run():
+    from pydj.playlist.models import SongDir
+
+    directory = os.path.join(settings.PROJECT_PATH, 'playlist/song_dir');
+    if not os.path.exists(directory):
+        print "Directory %s doesn't exists, please create." %dir
+        return
+
+    playlist_songdir_1 = SongDir()
+    playlist_songdir_1.path = directory
+    playlist_songdir_1.hash_letters = 0L
+    playlist_songdir_1.usable = True
+    playlist_songdir_1.save()
+
+    from pydj.playlist.models import Settings
+
+    playlist_settings_1 = Settings()
+    playlist_settings_1.key = u'welcome_message'
+    playlist_settings_1.value = u'0'
+    playlist_settings_1.save()
+
