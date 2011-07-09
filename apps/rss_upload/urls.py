5ddfe75ed59aebf3efda104ca66ee969a76763bf
Andrzej Bieniek <andyhelp@gmail.com>
2011-07-09 08:48:48 +0100

    Add rss entries list and upload option.

--- apps/rss_upload/urls.py
+++ apps/rss_upload/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^add',      'rss_upload.views.add'),
     (r'^list',     'rss_upload.views.list'),
+    (r'^entries/(\d+)', 'rss_upload.views.entries'),
     (r'^refresh',  'rss_upload.views.refresh'),
+    (r'^upload/(\d+)/(\d+)', 'rss_upload.views.upload'),
 )
--- apps/rss_upload/views.py
+++ apps/rss_upload/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, Http404
 from rss_upload.forms import RssUrlForm, RssRefreshForm
-from rss_upload.models import Rss
+from rss_upload.models import Rss, RssEntry
+import urllib
+import tempfile
 
 def list(request):
     rss_list = Rss.objects.filter(user=request.user)
     form = RssUrlForm()
     return render_to_response('rss_upload/list.html', {'rss_list': rss_list, 'form': form}, context_instance=RequestContext(request))
+
+def entries(request,rssid_str):
+    rssid = int(rssid_str)
+    rss_list = Rss.objects.filter(id=rssid)
+    rss = None
+    if rss_list:
+        rss = rss_list[0]
+    entry_list = RssEntry.objects.filter(rss=rssid)
+    return render_to_response('rss_upload/rss_entry_list.html', {'rss': rss, 'entry_list': entry_list}, context_instance=RequestContext(request))
+    
 
 
 def add(request):
@@ -29,4 +41,19 @@ def refresh(request):
         rss = Rss.objects.filter(id=id)
         if rss:
             rss[0].refresh()
+    return HttpResponseRedirect('/rss/list')
+
+
+def upload(request, rssid_str, entryid_str):
+    rssid = int(rssid_str)
+    entryid = int(entryid_str)
+    
+    rssEntry_list = RssEntry.objects.filter(id=entryid)
+    if rssEntry_list:
+        rssEntry = rssEntry_list[0]
+        f = tempfile.NamedTemporaryFile()
+        urllib.urlretrieve(rssEntry.track_url, f.name )        
+        request.user.get_profile().uploadSong(UploadedFile(f.name, filetype='mp3'))
+        f.close()
+    
     return HttpResponseRedirect('/rss/list')
